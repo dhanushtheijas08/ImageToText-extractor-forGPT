a694d29a179e3b836a580a7df464764f251db8f8
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-11-30 19:42:01 +0530

    created new btn ui

--- scripts/content.js
+++ scripts/content.js
@@ -18,6 +18,9 @@ const ELEMENT_IDS = {
   overLayer: "over-layer",
 };
 
+const BUTTONS = {
+  uploadButton: `<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M13.5 3H12H8C6.34315 3 5 4.34315 5 6V18C5 19.6569 6.34315 21 8 21H12M13.5 3L19 8.625M13.5 3V7.625C13.5 8.17728 13.9477 8.625 14.5 8.625H19M19 8.625V11.8125" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M17.5 21L17.5 15M17.5 15L20 17.5M17.5 15L15 17.5" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`,
+};
 let worker = null;
 
 async function addScript() {
@@ -112,7 +115,7 @@ function addPasteListener() {
 function addUploadButton(fileWorker) {
   let mainDiv = createMainDiv();
 
-  let btn = createButton("Upload image", async function (e) {
+  let btn = createButton("Upload file", async function (e) {
     e.preventDefault();
     fileInput.click();
   });
@@ -178,20 +181,46 @@ function createButton(text, clickHandler) {
   let btn = document.createElement("button");
   btn.id = ELEMENT_IDS.uploadButton;
   btn.title = "Upload image";
-  btn.innerHTML = text;
-  btn.style.width = "200px";
   btn.style.margin = "5px";
-  btn.style.padding = "5px";
-  btn.style.border = "none";
-  btn.style.backgroundColor = "#4CAF50";
-  btn.style.color = "white";
+  btn.style.padding = "8px 5px";
   btn.style.cursor = "pointer";
-  btn.style.display = "inline";
-  btn.style.fontSize = "16px";
+  btn.style.backgroundColor = "transparent";
+  btn.style.border = "1.1px solid #c5c5d2";
   btn.style.borderRadius = "8px";
-  btn.style.fontWeight = "bold";
-  btn.style.outline = "none";
+  btn.style.color = "#fff";
+  btn.style.fontSize = "16px";
+  btn.style.display = "flex";
+  btn.style.alignItems = "center";
+  btn.style.gap = "5px";
+  btn.innerHTML = text;
+
+  btn.addEventListener(
+    "mouseover",
+    () => (btn.style.backgroundColor = "#40414f")
+  );
+  btn.addEventListener(
+    "mouseleave",
+    () => (btn.style.backgroundColor = "transparent")
+  );
+  btn.insertAdjacentHTML("afterbegin", BUTTONS.uploadButton);
   btn.addEventListener("click", clickHandler);
+
+  // btn.style.width = "200px";
+  // btn.style.color = "white";
+  // btn.style.border = "none";
+  // btn.style.backgroundColor = "#4CAF50";
+  // btn.style.display = "inline";
+  // btn.style.borderRadius = "8px";
+  // btn.style.fontWeight = "bold";
+  // btn.style.outline = "none";
+  // outer button
+  // btn relative btn-neutral btn-small flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-lg border border-token-border-medium focus:ring-0 ml-2
+
+  // inner div
+  // flex w-full gap-2 items-center justify-center
+
+  // SVG
+  // <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="text-black dark:text-white"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.7929 2.79289C18.0118 1.57394 19.9882 1.57394 21.2071 2.79289C22.4261 4.01184 22.4261 5.98815 21.2071 7.20711L12.7071 15.7071C12.5196 15.8946 12.2652 16 12 16H9C8.44772 16 8 15.5523 8 15V12C8 11.7348 8.10536 11.4804 8.29289 11.2929L16.7929 2.79289ZM19.7929 4.20711C19.355 3.7692 18.645 3.7692 18.2071 4.2071L10 12.4142V14H11.5858L19.7929 5.79289C20.2308 5.35499 20.2308 4.64501 19.7929 4.20711ZM6 5C5.44772 5 5 5.44771 5 6V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V14C19 13.4477 19.4477 13 20 13C20.5523 13 21 13.4477 21 14V18C21 19.6569 19.6569 21 18 21H6C4.34315 21 3 19.6569 3 18V6C3 4.34314 4.34315 3 6 3H10C10.5523 3 11 3.44771 11 4C11 4.55228 10.5523 5 10 5H6Z" fill="currentColor"></path></svg>
 
   return btn;
 }
